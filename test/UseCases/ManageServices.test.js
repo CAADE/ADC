399b3f96dafbbc3d1a71e283ec3d9255d16eb249
madajaju <darren@pulsipher.org>
2017-10-02 20:19:56 -0700

    Add build file to the project.

---
+++ test/UseCases/ManageServices.test.js
@@ -0,0 +1,9 @@
+var Promise = require('bluebird');
+
+describe('Use Case ManageServices', function () {
+  describe('Primary Scenario', function () {
+    it('Test Scenario 1', function (done) {
+       done();
+    });
+  });
+});
